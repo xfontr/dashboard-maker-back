56cca6297be64d6c38f3dad1749e37c8f7e9a7b1
xfontr <xifre@Xifres-MacBook-Pro.local>
2022-12-26 15:33:05 +0100

    Set debug repository to serve debug with automatic message and chalk

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,4 +1,6 @@
+import "../loadEnvironment";
+
 export default {
   debug: process.env.DEBUG ?? "*",
-  port: +process.env.PORT ?? 4000,
+  port: (process.env.PORT as unknown as number) ?? 4_000,
 };
---
+++ src/services/setDebug/setDebug.ts
@@ -0,0 +1,46 @@
+import "../../loadEnvironment";
+import DebugFunction, { Debug, Debugger } from "debug";
+import environment from "../../config/environment";
+import setMessage from "../setMessage";
+import AvaliableColors from "../../types/avaliableColors";
+import CustomDebug from "../../types/customDebug";
+
+const importDebugAndSetBaseLocation = (
+  debug: Debug,
+  baseLocation: string,
+  callback: Function
+): Debug => callback(debug, baseLocation);
+
+const baseSetDebug =
+  (debug: Debug, baseLocation: string) =>
+  // eslint-disable-next-line arrow-body-style
+  (currentLocation: string): Debugger => {
+    return debug(`${baseLocation.slice(0, -2)}:${currentLocation}`);
+  };
+
+export const setDebugWithoutColors = importDebugAndSetBaseLocation(
+  DebugFunction,
+  environment.debug,
+  baseSetDebug
+);
+
+/**
+ * Imports Debug and sets the base location from the environment.
+ *
+ * @param {string} CurrentLocation Current file the user is at
+ *
+ * @returns {Debug} Returns a Debug function that when called will know the base location and import Debug by default
+ *
+ * @example
+ *
+ * const debug = setDebug("index")
+ * debug("Server listening at port 3000")
+ *
+ */
+
+export const setDebug = (currentLocation: string): CustomDebug => {
+  const debug = setDebugWithoutColors(currentLocation);
+
+  return (color: AvaliableColors, message: string): void =>
+    debug(setMessage(color, message));
+};
