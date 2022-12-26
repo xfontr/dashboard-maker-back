b3f21853f3bb69451d1cd3c0b1e02eb239e403a0
xfontr <xifre@Xifres-MacBook-Pro.local>
2022-12-26 12:16:34 +0100

    Install express and other necessary dependencies plus running server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const api = () => {};
+import "./loadEnvironment";
+import Debug from "debug";
+import chalk from "chalk";
+import express from "express";
+import environment from "./config/environment";
 
-export default api;
+const debug = Debug("dashboard-maker:index");
+
+const app = express();
+
+const startServer = () => {
+  app.listen(environment.port, () => {
+    debug(chalk.bgGreen(`Listening at port ${environment.port}`));
+  });
+};
+
+startServer();
