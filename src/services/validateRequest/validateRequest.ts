ad351305f53dc63ba6c713ecc85730ff04e55f38
xfontr <xifre@Xifres-MacBook-Pro.local>
2022-12-28 17:14:54 +0100

    Implement joi request validation

---
+++ src/services/validateRequest/validateRequest.ts
@@ -0,0 +1,15 @@
+import { RequestHandler } from "express";
+import { EvOptions, schema as Schema, validate } from "express-validation";
+
+const defaultAbortEarly = { abortEarly: false };
+
+export default (
+  (validator: Function) =>
+  (
+    schema: Schema,
+    options: EvOptions = {},
+    joiRoot: object = defaultAbortEarly
+  ): RequestHandler => {
+    return validator(schema, options, joiRoot);
+  }
+)(validate);
