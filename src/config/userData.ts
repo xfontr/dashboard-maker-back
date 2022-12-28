6357df0f489e6e20f88dad0c52986fbe6fa93bc6
xfontr <xifre@Xifres-MacBook-Pro.local>
2022-12-28 17:48:08 +0100

    Validation error middleware implementation and testing

---
+++ src/config/userData.ts
@@ -0,0 +1,11 @@
+export default {
+  name: {
+    min: 3,
+    max: 15,
+  },
+
+  email: {
+    min: 10,
+    max: 25,
+  },
+};
---
+++ src/middlewares/validationError/validationError.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { ValidationError } from "express-validation";
+import codes from "../../config/codes";
+import setDebug from "../../services/setDebug/setDebug";
+
+const debug = setDebug("validation-error");
+
+export default (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof ValidationError) {
+    debug("highError", `Validaton error: ${error.message}`);
+    error.details.body.forEach(({ message }) => debug("error", message));
+    res.status(codes.error.badRequest).json({ error: "Bad Request" });
+    return;
+  }
+
+  next(error);
+};
