c5916215267b95e85ce0f2f880065ebdb9e55fcb
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-02-19 11:13:53 +0100

    Refactor checkToken middleware to simpler function

---
+++ src/common/utils/isSignTokenValid.ts
@@ -0,0 +1,31 @@
+import { NextFunction } from "express";
+import { MAIN_IDENTIFIER } from "../../config/database";
+import { compareHash } from "../services/authentication";
+import catchCodedError from "./catchCodedError";
+import getBearerToken from "./getBearerToken";
+import userErrors from "../../modules/user/users.errors";
+import CustomRequest from "../types/CustomRequest";
+
+const isSignTokenValid = async (
+  req: CustomRequest,
+  next: NextFunction,
+  skip: boolean = false
+): Promise<Boolean> => {
+  if (skip || !req.token.isCodeRequired) return true;
+
+  const tryThis = catchCodedError(next);
+  const code = getBearerToken(req.headers?.authorization);
+
+  if (
+    !code ||
+    req.token[MAIN_IDENTIFIER] !== req.body[MAIN_IDENTIFIER] ||
+    !(await tryThis(compareHash, [code, req.token.code]))
+  ) {
+    next(userErrors.invalidToken);
+    return false;
+  }
+
+  return true;
+};
+
+export default isSignTokenValid;
