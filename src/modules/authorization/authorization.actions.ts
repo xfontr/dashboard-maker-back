a0ef5cf48c0ec1e19a883e8cb39a0e5314f1b43e
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-02-22 22:07:57 +0100

    Add base default actions

--- src/modules/authorization/authorization.actions.ts
+++ src/modules/authorization/authorization.actions.ts
@@ -2,27 +2,32 @@ import { CreateToken, GetAllUsers } from "./authorization.store";
 import { ActionName, ActionOptions } from "./authorization.types";
 import { UserRoles } from "../user/users.types";
 
+const BASE_ACTIONS: Record<ActionName, ActionOptions> = {
+  ...CreateToken({ isAuthorized: false, affectsUsers: [] }),
+  ...GetAllUsers({ isAuthorized: true }),
+};
+
 const AUTHORIZED_ACTIONS: Record<
   UserRoles,
   Record<ActionName, ActionOptions>
 > = {
   superAdmin: {
+    ...BASE_ACTIONS,
     ...CreateToken({
       isAuthorized: true,
       affectsUsers: ["user", "admin", "superAdmin"],
     }),
-    ...GetAllUsers({ isAuthorized: true }),
   },
   owner: {
+    ...BASE_ACTIONS,
     ...CreateToken({ isAuthorized: true, affectsUsers: ["user", "admin"] }),
-    ...GetAllUsers({ isAuthorized: true }),
   },
   admin: {
+    ...BASE_ACTIONS,
     ...CreateToken({ isAuthorized: true, affectsUsers: ["user"] }),
-    ...GetAllUsers({ isAuthorized: true }),
   },
   user: {
-    ...CreateToken({ isAuthorized: false }),
+    ...BASE_ACTIONS,
     ...GetAllUsers({ isAuthorized: false }),
   },
 };
